e876cd7a3b1d69f500f71dd2a014fb2fc77a2dd5
JAHID HASAN <developerjahid@gmail.com>
2023-11-15 22:33:21 +0600

    img icon showing by type of tha data like movies or music

--- src/Components/TorrantData/TorrentMoviesData.jsx
+++ src/Components/TorrantData/TorrentMoviesData.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import movies from './../../assets/all-icon/movies.png';
+import anime from './../../assets/all-icon/anime.png';
+import games from './../../assets/all-icon/games.png';
+import tutorials from './../../assets/all-icon/tutorials.png';
+import music from './../../assets/all-icon/music.png';
+import tv from './../../assets/all-icon/tv.png';
+import ebooks from './../../assets/all-icon/ebooks.png';
 import download from './../../assets/all-icon/download.png'
 
 const TorrentMoviesData = (props) => {
@@ -11,7 +17,15 @@ const TorrentMoviesData = (props) => {
             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td className="w-4 p-4">
                     <div className="flex items-center">
-                       <img className='h-14 w-14' src= {movies}></img>
+                    
+                    <img className={`h-14 w-14 ${type=="movies"? "" :"visibility: hidden" }`} src= {movies}></img>
+                    <img className={`h-14 w-14 ${type=="music"? "" :"visibility: hidden" }`} src= {music}></img>
+                    <img className={`h-14 w-14 ${type=="tutorial"? "" :"visibility: hidden" }`} src= {tutorials}></img>
+                    <img className={`h-14 w-14 ${type=="anime"? "" :"visibility: hidden" }`} src= {anime}></img>
+                    <img className={`h-14 w-14 ${type=="games"? "" :"visibility: hidden" }`} src= {games}></img>
+                    <img className={`h-14 w-14 ${type=="tv"? "" :"visibility: hidden" }`} src= {tv}></img>
+                    <img className={`h-14 w-14 ${type=="ebooks"? "" :"visibility: hidden" }`} src= {ebooks}></img>
+                    
                     </div>
                 </td>
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
